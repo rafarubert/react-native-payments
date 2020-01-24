3e983cc5d8c93fb1804914120f3ec885c328bab7
Ain <ain@abcd.sa>
2020-01-24 18:57:59 +0300

    Remove broken fields

--- packages/react-native-payments/lib/js/PaymentRequest.js
+++ packages/react-native-payments/lib/js/PaymentRequest.js
@@ -379,11 +379,11 @@ export default class PaymentRequest {
       shippingAddress: this._options.requestShipping ? this._shippingAddress : null,
       details: this._getPlatformDetails(details),
       shippingOption: IS_IOS ? this._shippingOption : null,
-      payerName: this._options.requestPayerName ? this._shippingAddress.recipient : null,
-      payerPhone: this._options.requestPayerPhone ? this._shippingAddress.phone : null,
-      payerEmail: IS_ANDROID && this._options.requestPayerEmail
-        ? details.payerEmail
-        : null
+      // payerName: this._options.requestPayerName ? this._shippingAddress.recipient : null,
+      // payerPhone: this._options.requestPayerPhone ? this._shippingAddress.phone : null,
+      // payerEmail: IS_ANDROID && this._options.requestPayerEmail
+      //   ? details.payerEmail
+      //   : null
     });
 
     return this._acceptPromiseResolver(paymentResponse);
